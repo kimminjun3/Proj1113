0b98f3333dc28c12907a324e62a621a2daadcc9f
minjun <ivy0987@naver.com>
2021-06-24 16:40:20 +0900

    Proj1113

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proj1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
